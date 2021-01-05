e8f815256e8f469449e8482f7cf316c95b5cca9d
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2021-01-05 19:05:58 +0100

    Initial commit
    Add mongoose and mongodb for the database
    Add dotenv for the app to read .env files
    Connect to the database hosted in mongodb atlas

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
-
+require('dotenv').config({path: __dirname + "/.env"});
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
